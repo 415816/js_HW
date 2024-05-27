ab1f15287064d61559909f246e432f8340cc7ed7
Vitaly <415816@bk.ru>
2024-05-28 01:44:04 +0500

    deleted odd lines and comments

--- lec_3_HW/task_2.js
+++ lec_3_HW/task_2.js
@@ -1,20 +1,20 @@
 let x;
 let result;
-// let D = b^2 - 4*a*c;
-
 
 const getSolution = (a, b, c) => {
-    let D = b^2 - 4*a*c;
-if (D > 0) {
-    x = (-b + Math.sqrt(D)) / (2 * a);
-    result = `x1 = ${x}`;
-    x = (-b - Math.sqrt(D)) / (2 * a);
-    return result += `, x2 = ${x}`;
-} else if (D === 0) {
-    x = -b / (2 * a);
-    return result = `x = ${x}`;
-} else {
-    return result = "No roots";
-}
-}
-console.log(`Ответ к уравнению 1: ${getSolution(1, -6, 9)}, Ответ к уравнению 2: ${getSolution(1, -4, -5)}`);
+    let D = b ^ (2 - 4 * a * c);
+    if (D > 0) {
+        x = (-b + Math.sqrt(D)) / (2 * a);
+        result = `x1 = ${x}`;
+        x = (-b - Math.sqrt(D)) / (2 * a);
+        return (result += `, x2 = ${x}`);
+    } else if (D === 0) {
+        x = -b / (2 * a);
+        return (result = `x = ${x}`);
+    } else {
+        return (result = "No roots");
+    }
+};
+console.log(
+    `Ответ к уравнению 1: ${getSolution(1, -6, 9)}, Ответ к уравнению 2: ${getSolution(1, -4, -5)}`
+);
--- lec_3_HW/task_3.js
+++ lec_3_HW/task_3.js
@@ -1,12 +1,9 @@
-
-
-const sumCancat = n => {
+const sumCancat = (n) => {
     let first = n;
     let second = "" + n + n;
-    let third = ""+ n + n + n;
+    let third = "" + n + n + n;
 
     console.log(first + +second + +third);
 };
-
 
 sumCancat(2);
