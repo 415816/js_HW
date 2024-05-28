a491d3c10f116519118d8199494752c7ccc31f76
Vitaly <415816@bk.ru>
2024-05-28 21:31:06 +0500

    complited HW for 4th lecture

---
+++ lec_3_HW/task_2.js
@@ -0,0 +1,18 @@
+let x;
+let result;
+
+const getSolution = (a, b, c) => {
+    let D = b ^ (2 - 4 * a * c);
+    if (D > 0) {
+        x = (-b + Math.sqrt(D)) / (2 * a);
+        result = `x1 = ${x}`;
+        x = (-b - Math.sqrt(D)) / (2 * a);
+        return (result += `, x2 = ${x}`);
+    } else if (D === 0) {
+        x = -b / (2 * a);
+        return (result = `x = ${x}`);
+    } else {
+        return (result = "No roots");
+    }
+};
+console.log(`Ответ к уравнению 1: ${getSolution(1, -6, 9)}, Ответ к уравнению 2: ${getSolution(1, -4, -5)}`);
---
+++ lec_4_HW/task_3.js
@@ -0,0 +1,25 @@
+let minAge = 18;
+            let maxAge = 60;
+
+            const checkAge = age => {
+                if (+age === NaN) {
+                    alert("Please, enter a number");
+                } else {
+                    switch (true) {
+                        case minAge > +age:
+                            alert(`You don't have access cause your age is ${age}, it's less than ${minAge}`);
+                            break;
+                        case +age >= minAge && +age < maxAge:
+                            alert(`Welcome     !`);
+                            break;
+                        case +age > maxAge:
+                            alert("Keep calm and look Culture channel");
+                            break;
+                        default:
+                            alert("Technical work");
+                    }
+                }
+            };
+
+            const ageStr = prompt("Please enter your age:");
+            checkAge(Number(ageStr));
