fc5b7bf8de0efb7a46c0f3c44824354eb5d695c5
Vitaly <415816@bk.ru>
2024-06-23 14:39:15 +0500

    added 6th HW

--- lec_6_HW/task_3.js
+++ lec_6_HW/task_3.js
@@ -13,19 +13,31 @@ const sumDigits = num => {
 // console.log(sumDigits(123));
 // console.log(sumDigits(19));
 
-const replaceSimilarLetters = str => {
-    let newStr = "";
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === str[i + 1]) {
-            newStr += String.fromCharCode(str.charCodeAt(i) + 1);
-            i++;
-            console.log(newStr);
-        } else {
-            newStr += str[i];
-        }
-    }
-    return newStr;
-};
+// const replaceSimilarLetters = str => {
+//     let newStr = "";
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] === str[i + 1]) {
+//             newStr += String.fromCharCode(str.charCodeAt(i) + 1);
+//             i++;
+//             console.log(newStr);
+//         } else {
+//             newStr += str[i];
+//         }
+//     }
+//     return newStr;
+// };
 
-// console.log(replaceSimilarLetters("abbcdd"));
+
+function nextChar(c) {
+    return c === 'z' ? 'a' : String.fromCharCode(c.charCodeAt(0) + 1);
+}
+
+function replaceSimilarLetters(str) {
+    let pattern = /(.)\1/;
+    while (pattern.test(str)) {
+        str = str.replace(pattern, (match, p1) => nextChar(p1));
+    }
+    return str;
+}
+
 console.log(replaceSimilarLetters("aabc"));
