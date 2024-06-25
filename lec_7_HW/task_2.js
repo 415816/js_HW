2efa920cf619bfaa8fb27a673bc5c58dfffc6bc5
Vitaly <415816@bk.ru>
2024-06-26 00:32:16 +0500

    added 7th HW

---
+++ lec_7_HW/task_2.js
@@ -0,0 +1,74 @@
+const str = "I am the best AQA ever!";
+
+function replaceLetterToCount(str) {
+    let count = {};
+
+    str = str.toLowerCase();
+
+    for (let char of str) {
+        if (char.match(/[a-z]/i)) {
+            count[char] = (count[char] || 0) + 1;
+        }
+    }
+
+    let result = "";
+    for (let char of str) {
+        if (char.match(/[a-z]/i)) {
+            result += count[char];
+        } else {
+            result += char;
+        }
+    }
+
+    return result;
+}
+
+let output = replaceLetterToCount(str);
+// console.log(output);
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const prices = [
+    64, 7556, 345, 7556, 345, 7556, 345, 7556, 433, 345, 765, 123, 942, 3112, 421, 9341, 1212, 8, 43, 41, 345, 341, 21,
+    321, 123,
+];
+
+const actionPrices = prices => {
+    let sum = 0;
+    let average = 0;
+    let counter = 0;
+
+    prices.forEach(price => {
+        sum += price;
+        counter++;
+    });
+    average = sum / counter;
+    return `Итого: ${sum} $, средняя цена товара: ${average} $`;
+};
+// console.log(actionPrices(prices));
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const sortByVowelCount = words => {
+    function countVowels(word) {
+        const vowels = /[aeiou]/g;
+        let matches = word.toLowerCase().match(vowels);
+        return matches ? matches.length : 0;
+    }
+
+    return words.sort((a, b) => countVowels(a) - countVowels(b));
+};
+
+let words = ["one", "two", "three", "eleven", "fourteen"];
+let sortedWords = sortByVowelCount(words);
+// console.log(sortedWords);
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function countBrackets(arr) {
+    let flatArray = arr.flat(Infinity);
+    let openCount = flatArray.filter(char => char === "(").length;
+    let closeCount = flatArray.filter(char => char === ")").length;
+
+    return openCount === closeCount;
+}
+
+let bracketsArrayFalse = ["(", ["("], ")", "(", "(", "(", [[")"], ")"], ")", ")", ")"];
+let bracketsArrayTrue = ["(", ["("], ")", "(", "(", "(", [[")"], ")"], ")", ")"];
+
+console.log(countBrackets(bracketsArrayFalse));
+console.log(countBrackets(bracketsArrayTrue));
